a2e034822b50e9e359fe559c38848a34c375541d
Jassim Mohammed Shamim <72601716+jazxii@users.noreply.github.com>
2021-05-05 17:24:02 +0530

    updates in code 1

--- app/src/main/java/com/example/davincipacker/Home.kt
+++ app/src/main/java/com/example/davincipacker/Home.kt
@@ -1,11 +1,27 @@
 package com.example.davincipacker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+
+        val pl : Button = findViewById(R.id.plan)
+        pl.setOnClickListener {
+            val intent = Intent(this, Qstn1::class.java)
+            startActivity(intent)
+        }
+
+
+
+
     }
+
+
+
 }
