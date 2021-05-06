1f88098339fc060c079fcc74c06aa19ffcdc6c6b
Jassim Mohammed Shamim <72601716+jazxii@users.noreply.github.com>
2021-05-06 21:54:08 +0530

    updates in code 1

--- app/src/main/java/com/example/davincipacker/Qstn2.kt
+++ app/src/main/java/com/example/davincipacker/Qstn2.kt
@@ -7,5 +7,7 @@ class Qstn2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qstn2)
+
+
     }
 }
